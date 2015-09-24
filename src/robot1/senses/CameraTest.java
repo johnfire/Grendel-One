1844b7947e69623bbd6a8074ef048d03f3ea1aea
chrisrehm <christopherrehm@web.de>
2015-09-24 15:26:46 +0200

    updated files, with tighter code.

--- src/robot1/senses/CameraTest.java
+++ src/robot1/senses/CameraTest.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package robot1.senses;
 
 import robot1.senses.RaspiStill;
@@ -11,34 +12,31 @@ import robot1.senses.RaspiStill;
  *
  * @author robert mcmurray
  */
+
 public class CameraTest {
     // Define the number of photos to take.
-  private static final long _numberOfImages = 5;
-  // Define the interval between photos.
-  private static final int _delayInterval = 5000;
+    private static final long _numberOfImages = 5;
+    // Define the interval between photos.
+    private static final int _delayInterval = 5000;
 
-  public static void main(String[] args){
-          }
-  public static void takePic (){
-  {
-    try
-    {
-      // Create a new RaspiStill object.
-      RaspiStill camera = new RaspiStill();
-      // Loop through the number of images to take.
-      for (long i = 0; i < _numberOfImages; ++i)
-      {
-        // Capture the image.
-        camera.TakePicture(System.currentTimeMillis() + ".jpg",800,600);
-        // Pause after each photo.
-        Thread.sleep(_delayInterval);
-      }
-    }
-    catch (Exception e)
-    {
-      // Exit the application with the exception's hash code.
-      System.exit(e.hashCode());
-    }
+    public static void main(String[] args){
+      
   }
-    
+  
+    public static void takePic (){
+        try{
+            // Create a new RaspiStill object.
+            RaspiStill camera = new RaspiStill();
+            // Loop through the number of images to take.
+            for (long i = 0; i < _numberOfImages; ++i){
+                // Capture the image.
+                camera.TakePicture(System.currentTimeMillis() + ".jpg",800,600);
+                // Pause after each photo.
+                Thread.sleep(_delayInterval);
+            }
+        }catch (Exception e){
+            // Exit the application with the exception's hash code.
+            System.exit(e.hashCode());
+        }
+    }  
 }
